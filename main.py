b1ff80b9642b4b077eb37f10db2330f358ddada6
AftabAhmed4 <mt2348219@gmail.com>
2025-07-17 22:58:32 +0500

    cli base agent

--- main.py
+++ main.py
@@ -1,8 +1,56 @@
+import os
 import chainlit as cl
+from agents import (
+    AsyncOpenAI,
+    OpenAIChatCompletionsModel,
+    RunConfig,
+    Agent,
+    Runner
+)
+
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+gemini_api_key =os.getenv("GEMINI_API_KEY")
+
+
+# step # 1 set up provider
+provider = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+# setup # 2 modal
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash", 
+    openai_client=provider
+)
 
 
 
-@cl.on_message
-async def main (message: cl.Message):
-    response: str =f"Hello, {message.content}"
-    await cl.Message(response).send()
+# step # 3 configuration 
+config = RunConfig(
+    model_provider= provider,
+    model=model,
+    tracing_disabled=True
+)
+
+
+# step # 4 Agent
+agent = Agent(
+    name="smart student agent",
+    instructions="You are smart full agent"
+
+)
+
+
+
+user_input = input("Enter Query: ")
+
+ressult = Runner.run_sync(
+    agent,
+    input=user_input,
+    run_config=config
+)
+
+print(ressult.final_output)
